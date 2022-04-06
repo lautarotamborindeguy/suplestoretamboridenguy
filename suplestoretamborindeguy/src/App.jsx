456c17480beb5700512072ef645eb0040569e600
LautaroTamborindeguy <lautarotegui4@gmail.com>
2022-04-05 23:26:15 -0300

    Commit correspondiente a la clase de context. Aún no logré que funcione

--- suplestoretamborindeguy/src/App.jsx
+++ suplestoretamborindeguy/src/App.jsx
@@ -6,19 +6,29 @@ import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { useState } from 'react';
+import {CartContext} from './context/CartContext'
+
 function App() {
+  const [productos, setProductos] = useState([]);
+    
+  const addProductos = (newProduct)=> {
+    setProductos([...productos, newProduct])
+  }
   return ( 
-    <BrowserRouter>
-        <NavBar></NavBar>
-        <Routes>
-          <Route path="/"  element={<ItemListContainer />} />
-          <Route path="/productos/:categoryId" element={ <ItemListContainer/>}/>
-          <Route path="/detail/:itemId" element={<ItemDetailContainer/>}/>
-          <Route path="/detalles"  element={<ItemDetailContainer />} />
-          <Route path="/cart"/>
-          <Route path="*" element={<Navigate to="/"/>}/>
-        </Routes>
-    </BrowserRouter>
+    <CartContext.Provider value={{productos, addProductos}}>
+      <BrowserRouter>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path="/"  element={<ItemListContainer />} />
+            <Route path="/productos/:categoryId" element={ <ItemListContainer/>}/>
+            <Route path="/detail/:itemId" element={<ItemDetailContainer/>}/>
+            <Route path="/detalles"  element={<ItemDetailContainer />} />
+            <Route path="/cart"/>
+            <Route path="*" element={<Navigate to="/"/>}/>
+          </Routes>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
