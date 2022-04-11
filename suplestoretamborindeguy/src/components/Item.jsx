fe44cd0299e1bd87fe82981ea1a69a04673a5657
Lautaro Tamborindeguy <lautarotegui4@gmail.com>
2022-04-11 19:06:25 -0300

    Cambios en el maquetado

--- suplestoretamborindeguy/src/components/Item.jsx
+++ suplestoretamborindeguy/src/components/Item.jsx
@@ -5,10 +5,10 @@ const Item = ({producto}) =>  {
     let {id, nombre, precio, urlImages, altImages, categoria, stock} = producto
     return (
         <div>
-                <div className="mt-4 containerCart p-2">
-                    <div className="d-flex justify-content-between align-items-center">
+                <div className="mt-4 containerCart">
+                    <div className="d-flex justify-content-between align-items-center top-item bg-gradient">
                         <h2 className="titleArticleCart">{nombre}</h2>
-                        <p className="fw-bold mb-0">${precio}</p>
+                        <p className="fw-bold mb-0 text-white">${precio}</p>
                     </div>
                     <div className="d-flex align-items-center">
                         <img src={urlImages} alt={altImages} className="imagesCart"/>
